7410dafb74444bf5dd923eff93cbf4fa85ba6838
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-05-23 16:37:07 -0700

    seed data mapping in saved books page

--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import API from "../../utils/API";
 
 function Saved() {
-    return(
-        <h1>Saved Page</h1>
-    )
+
+    const [books, setBooks] = useState([])
+  
+    // Load all books and store them with setBooks
+    useEffect(() => {
+      loadBooks()
+    }, [])
+  
+    // Loads all books and sets them to books
+    function loadBooks() {
+      API.getBooks()
+        .then(res => 
+          setBooks(res.data)
+        )
+        .catch(err => console.log(err));
+    };
+  
+    return (
+      <div className="container-fluid">
+          <div className="row">
+              <div className="hero">
+                  <h1>All Books From Mongo DB</h1>
+              </div>
+
+              {books.length ? (
+
+              <div className="cardDeck">
+              {books.map(book => {
+                  return (
+                    <div className="card" key={book._id}>
+                      <h4>
+                        {book.title} by {book.author}
+                      </h4>
+                      <li>
+                        {book.description}
+                      </li>
+                      <li>
+                        {book.image}
+                      </li>
+                      <li>
+                        {book.link}
+                      </li>
+                    </div>
+                  );
+              })}
+              </div>
+              ) : (
+              <h3>No Results to Display</h3>
+            )}
+        </div>
+      </div>
+    );   
 };
 
 export default Saved;
--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import API from "../../utils/API";
 
 function Search() {
     return(
